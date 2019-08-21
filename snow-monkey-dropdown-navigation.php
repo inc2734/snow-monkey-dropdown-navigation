c2532412f329cc020ffaa060cf3edf4ab4bbc7db
Takashi Kitajima <inc@2inc.org>
2019-08-21 22:44:33 +0900

    Refactoring admin notices

--- snow-monkey-dropdown-navigation.php
+++ snow-monkey-dropdown-navigation.php
@@ -27,8 +27,18 @@ class Bootstrap {
 
 		add_action( 'init', [ $this, '_activate_autoupdate' ] );
 
+		$theme = wp_get_theme( get_template() );
+		if ( 'snow-monkey' !== $theme->template && 'snow-monkey/resources' !== $theme->template ) {
+			add_action( 'admin_notices', [ $this, '_admin_notice_no_snow_monkey' ] );
+			return;
+		}
+
+		if ( ! version_compare( $theme->get( 'Version' ), '7.9.0', '>=' ) ) {
+			add_action( 'admin_notices', [ $this, '_admin_notice_invalid_snow_monkey_version' ] );
+			return;
+		}
+
 		new Controller\Front();
-		new Controller\Admin();
 	}
 
 	/**
@@ -42,6 +52,36 @@ class Bootstrap {
 			'inc2734',
 			'snow-monkey-dropdown-navigation'
 		);
+	}
+
+	/**
+	 * Admin notice for no Snow Monkey
+	 *
+	 * @return void
+	 */
+	public function _admin_notice_no_snow_monkey() {
+		?>
+		<div class="notice notice-warning is-dismissible">
+			<p>
+				<?php esc_html_e( '[Snow Monkey Dropdown Navigation] Needs the Snow Monkey.', 'snow-monkey-dropdown-navigation' ); ?>
+			</p>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Admin notice for invalid Snow Monkey version
+	 *
+	 * @return void
+	 */
+	public function _admin_notice_invalid_snow_monkey_version() {
+		?>
+		<div class="notice notice-warning is-dismissible">
+			<p>
+				<?php esc_html_e( '[Snow Monkey Dropdown Navigation] Needs the Snow Monkey v7.9 or more.', 'snow-monkey-dropdown-navigation' ); ?>
+			</p>
+		</div>
+		<?php
 	}
 }
 
