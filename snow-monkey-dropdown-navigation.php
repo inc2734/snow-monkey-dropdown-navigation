26529b2d4e35c41c914fdbd0e515a0a18e50c054
inc2734 <inc@2inc.org>
2021-08-27 16:01:34 +0900

    2.0.0

--- snow-monkey-dropdown-navigation.php
+++ snow-monkey-dropdown-navigation.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: Snow Monkey Dropdown Navigation
  * Description: Activating this plug-in changes the drawer navigation to drop navigation.
- * Version: 1.0.2
+ * Version: 2.0.0
  * Tested up to: 5.8
  * Requires at least: 5.5
  * Requires PHP: 5.6
