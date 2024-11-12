d28b62eaa7fbdbc454ac6e8904bdb21a0abe38a5
inc2734 <inc@2inc.org>
2024-11-12 15:50:46 +0900

    #29 load_plugin_textdomain() を init から後に変更

--- snow-monkey-dropdown-navigation.php
+++ snow-monkey-dropdown-navigation.php
@@ -34,8 +34,7 @@ class Bootstrap {
 	 * Plugins loaded.
 	 */
 	public function _plugins_loaded() {
-		load_plugin_textdomain( 'snow-monkey-dropdown-navigation', false, basename( __DIR__ ) . '/languages' );
-
+		add_action( 'init', array( $this, '_load_textdomain' ) );
 		add_action( 'init', array( $this, '_activate_autoupdate' ) );
 
 		$theme = wp_get_theme( get_template() );
@@ -98,6 +97,13 @@ class Bootstrap {
 				new Controller\Front();
 			}
 		);
+	}
+
+	/**
+	 * Load textdomain.
+	 */
+	public function _load_textdomain() {
+		load_plugin_textdomain( 'snow-monkey-dropdown-navigation', false, basename( __DIR__ ) . '/languages' );
 	}
 
 	/**
